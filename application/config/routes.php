be1d43823a8e3570215c0b15d6fd23d06e698c2a
lancestrom <dikakewren@gmail.com>
2025-11-10 11:46:50 +0700

    Update routes.php

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,6 @@
 <?php
 defined('BASEPATH') or exit('No direct script access allowed');
 
-$route['default_controller'] = 'CekAbsen';
+$route['default_controller'] = 'Login';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
